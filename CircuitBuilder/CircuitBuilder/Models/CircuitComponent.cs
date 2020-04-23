f67aa1f2a115f7ab4c915a0dcd7fee65216fbbd7
pab15 <54556870+pab15@users.noreply.github.com>
2020-04-22 18:45:07 -0700

    Started MVC Files

---
+++ CircuitBuilder/CircuitBuilder/Models/CircuitComponent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CircuitBuilder.Models
+{
+    public class CircuitComponent
+    {
+        private string componentType { get; set; }
+        private List<int> componentPosition { get; set; }
+
+        public CircuitComponent()
+        {
+            this.componentType = null;
+            this.componentPosition = null;
+        }
+        public CircuitComponent(string componentType, List<int> componentPosition)
+        {
+            this.componentType = componentType;
+            this.componentPosition = componentPosition;
+        }
+        public void setComponentType(string componentType)
+        {
+            this.componentType = componentType;
+        }
+        public string getComponentType()
+        {
+            return this.componentType;
+        }
+        public void setComponentPosition(List<int> componentPosition)
+        {
+            this.componentPosition = componentPosition;
+        }
+        public List<int> getComponentPosition()
+        {
+            return this.componentPosition;
+        }
+    }
+}
---
+++ CircuitBuilder/CircuitBuilder/Models/CircuitGraph.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CircuitBuilder.Models
+{
+    public class CircuitGraph
+    {
+        // Component Property:
+        // First --> Position of Component
+        // Second --> Component Object
+        // Dictionary Allows Us to Quickly Check if a Position is Taken.
+        private Dictionary<List<int>, CircuitComponent> components { get; set; }
+
+        // Connection Property:
+        // First --> Circuit Component
+        // Second --> All Adjacent Circuit Components
+        private Dictionary<CircuitComponent, List<CircuitComponent>> connections { get; set; }
+
+
+        public void addComponent(string componentType, List<int> componentPosition)
+        {
+            if (this.components.ContainsKey(componentPosition))
+            {
+                this.components[componentPosition] = new CircuitComponent(componentType, componentPosition);
+            }
+            else
+            {
+                this.components.Add(componentPosition, new CircuitComponent(componentType, componentPosition));
+            }
+        }
+        public void removeComponent(List<int> componentPosition)
+        {
+            if (this.components.ContainsKey(componentPosition))
+            {
+                this.components.Remove(componentPosition);
+            }
+        }
+    }
+}
